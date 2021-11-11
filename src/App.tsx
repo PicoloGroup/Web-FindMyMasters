b315608c8b92acf8a1e4b2658e77a201c6a6ae57
Hasan Can Aslan <aslan4044@gmail.com>
2021-11-11 22:45:11 +0300

    feat: add auth containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApplicationPage from "./ApplicationPage";
+import AuthContainer from './auth/AuthContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <AuthContainer />;
+  // return <ApplicationPage />;
+};
 
 export default App;
